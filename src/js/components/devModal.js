efceb279b924eca4eaa210470da66aab5a6d1676
namhels <8.lsv.888@gmail.com>
2022-12-29 22:05:42 +0300

    fix devModal

--- src/js/components/devModal.js
+++ src/js/components/devModal.js
@@ -1,14 +1,14 @@
-const bannerRef = document.getElementsByClassName('banner')[0];
-const blocksRef = document.getElementsByClassName('blocks');
-const sectionRef = document.querySelector('.dev-modal');
+// const bannerRef = document.getElementsByClassName('banner')[0];
+// const blocksRef = document.getElementsByClassName('blocks');
+// const sectionRef = document.querySelector('.dev-modal');
 
-for (var i = 1; i < 400; i++) {
-  bannerRef.innerHTML += "<div class='blocks'></div>";
-  const duration = Math.random() * 5;
-  blocksRef[i].style.animationDuration = 2 + duration + 's';
-  blocksRef[i].style.animationDelay = duration + 's';
-}
+// for (var i = 1; i < 400; i++) {
+//   bannerRef.innerHTML += "<div class='blocks'></div>";
+//   const duration = Math.random() * 5;
+//   blocksRef[i].style.animationDuration = 2 + duration + 's';
+//   blocksRef[i].style.animationDelay = duration + 's';
+// }
 
-setTimeout(() => {
-  sectionRef.classList.add('active');
-}, 14000);
+// setTimeout(() => {
+//   sectionRef.classList.add('active');
+// }, 14000);
